7d7525f5f55cd359a5f41551162812a03fa74f88
Christian Goehl <christian.goehl@gmx.net>
2025-06-13 09:37:59 +0200

    Provide TimeProc explicitly to OpenInput but not really solving the issue.

--- src/PortMidi/MidiDeviceManager.cs
+++ src/PortMidi/MidiDeviceManager.cs
@@ -6,64 +6,75 @@ using PmError = PortMidi.MidiErrorType;
 
 namespace PortMidi
 {
-    public static class MidiDeviceManager
-    {
-        public static int DefaultBufferSize { get; set; } = 1024;
+	public static class MidiDeviceManager
+	{
+		public static int DefaultBufferSize { get; set; } = 1024;
 
-        static MidiDeviceManager()
-        {
-            PortMidiMarshal.Pm_Initialize();
-            AppDomain.CurrentDomain.DomainUnload += delegate { PortMidiMarshal.Pm_Terminate(); };
-        }
+		static MidiDeviceManager()
+		{
+			PortMidiMarshal.Pm_Initialize();
+			AppDomain.CurrentDomain.DomainUnload += delegate { PortMidiMarshal.Pm_Terminate(); };
+		}
 
-        public static int DeviceCount => PortMidiMarshal.Pm_CountDevices();
+		public static int DeviceCount => PortMidiMarshal.Pm_CountDevices();
 
-        public static int DefaultInputDeviceId => PortMidiMarshal.Pm_GetDefaultInputDeviceID();
+		public static int DefaultInputDeviceId => PortMidiMarshal.Pm_GetDefaultInputDeviceID();
 
-        public static int DefaultOutputDeviceId => PortMidiMarshal.Pm_GetDefaultOutputDeviceID();
+		public static int DefaultOutputDeviceId => PortMidiMarshal.Pm_GetDefaultOutputDeviceID();
 
-        public static IEnumerable<MidiDeviceInfo> AllDevices
-        {
-            get
-            {
-                for (var i = 0; i < DeviceCount; i++)
-                {
-                    yield return GetDeviceInfo(i);
-                }
-            }
-        }
+		public static IEnumerable<MidiDeviceInfo> AllDevices
+		{
+			get
+			{
+				for (var i = 0; i < DeviceCount; i++)
+				{
+					yield return GetDeviceInfo(i);
+				}
+			}
+		}
 
-        private static MidiDeviceInfo GetDeviceInfo(PmDeviceID id)
-        {
-            return new MidiDeviceInfo(id, PortMidiMarshal.Pm_GetDeviceInfo(id));
-        }
+		private static MidiDeviceInfo GetDeviceInfo(PmDeviceID id)
+		{
+			return new MidiDeviceInfo(id, PortMidiMarshal.Pm_GetDeviceInfo(id));
+		}
 
-        public static MidiInput OpenInput(PmDeviceID inputDevice)
-        {
-            return OpenInput(inputDevice, DefaultBufferSize);
-        }
+		public static MidiInput OpenInput(PmDeviceID inputDevice)
+		{
+			return OpenInput(inputDevice, DefaultBufferSize);
+		}
 
-        private static MidiInput OpenInput(PmDeviceID inputDevice, int bufferSize)
-        {
-            PortMidiStream stream = default;
-            var e = PortMidiMarshal.Pm_OpenInput(out stream, inputDevice, IntPtr.Zero, bufferSize, null, IntPtr.Zero);
-            if (e != PmError.NoError)
-            {
-                throw new MidiException(e, $"Failed to open MIDI input device {e}");
-            }
-            
-            return new MidiInput(stream, inputDevice);
-        }
+		private static MidiInput OpenInput(PmDeviceID inputDevice, int bufferSize)
+		{
+			PortMidiStream stream = default;
+			var e = PortMidiMarshal.Pm_OpenInput(out stream, inputDevice, IntPtr.Zero, bufferSize, TimeProcDelegate,
+				IntPtr.Zero);
+			if (e != PmError.NoError)
+			{
+				throw new MidiException(e, $"Failed to open MIDI input device {e}");
+			}
 
-        public static MidiOutput OpenOutput(PmDeviceID outputDevice, int latency = 0)
-        {
-            PortMidiStream stream;
-            var e = PortMidiMarshal.Pm_OpenOutput(out stream, outputDevice, IntPtr.Zero, DefaultBufferSize, null, IntPtr.Zero, latency);
-            if (e != PmError.NoError)
-            {
-                throw new MidiException(e, $"Failed to open MIDI output device {e}");
-            }
-            return new MidiOutput(stream, outputDevice, 0);
-        }
-    }
+			return new MidiInput(stream, inputDevice);
+		}
+
+		private static MidiTimeProcDelegate TimeProcDelegate = TimeProc;
+
+		private static int TimeProc(IntPtr timeinfo)
+		{
+			return MidiTimer.Time();
+		}
+
+		public static MidiOutput OpenOutput(PmDeviceID outputDevice, int latency = 0)
+		{
+			PortMidiStream stream;
+			var e = PortMidiMarshal.Pm_OpenOutput(out stream, outputDevice, IntPtr.Zero, DefaultBufferSize, TimeProcDelegate,
+				IntPtr.Zero,
+				latency);
+			if (e != PmError.NoError)
+			{
+				throw new MidiException(e, $"Failed to open MIDI output device {e}");
+			}
+
+			return new MidiOutput(stream, outputDevice, 0);
+		}
+	}
 }
